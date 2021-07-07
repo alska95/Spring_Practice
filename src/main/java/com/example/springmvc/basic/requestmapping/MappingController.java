fabde2b9c4f3d69d517dfd9edb339cb8d3dac22c
alska95 <rhsalska55@naver.com>
2021-07-07 21:32:40 +0900

    요청 메핑 api 연습 / get , post , patch , delete

--- src/main/java/com/example/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/com/example/springmvc/basic/requestmapping/MappingController.java
@@ -17,10 +17,18 @@ public class MappingController {
         log.debug("hello basic2");
         return "iokPOost";
     }
-
+/*
+* pathVariable -> requestmapping  템플릿화*/
     @GetMapping("/mapping/{userId}")
     public String mappingPath(@PathVariable("userId") String data){
         log.info("mappingPath userId = {}" , data);
         return "okok";
+    }
+    /**
+     * pathVariable 다중 사용*/
+    @GetMapping("/mapping/user/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId , @PathVariable String orderId){
+        log.debug("mappingPath userId = {} , orderId = {}", userId , orderId );
+        return "ok";
     }
 }
