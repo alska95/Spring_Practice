58877fd30dfac94b9794afa424d566a507e9e35d
alska95 <rhsalska55@naver.com>
2021-07-10 12:10:46 +0900

    직접 만든 세션 적용 / UUID 작동 확인

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,39 +12,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-
+    private final SessionManager sessionManager;
     private final MemberRepository memberRepository;
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
+//    @GetMapping("/")
+//    public String homeLogin(@CookieValue(name = "memberId" , required = false) Long memberId , Model model){
+//        if(memberId == null){
+//            return "home";
+//        }
+//        //로그인
+//        Member loginMember = memberRepository.findById(memberId);
+//        if(loginMember == null){
+//            return "home";
+//        }
+//
+//        model.addAttribute("member" , loginMember);
+//        return "loginHome";
+//    }
     @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId" , required = false) Long memberId , Model model){
-        if(memberId == null){
+    public String homeLoginV2(HttpServletRequest request, Model model){
+        Member session = (Member)sessionManager.getSession(request);
+        if(session == null){
             return "home";
         }
-        //로그인
-        Member loginMember = memberRepository.findById(memberId);
-        if(loginMember == null){
-            return "home";
-        }
-
-        model.addAttribute("member" , loginMember);
+        model.addAttribute("member" , session);
         return "loginHome";
-    }
-
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response){
-        Cookie cookie = new Cookie("memberId", null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-        return "redirect:/";
     }
 }
