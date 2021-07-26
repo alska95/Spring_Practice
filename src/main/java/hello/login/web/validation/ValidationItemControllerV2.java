6e796aa311ec59cf41c35ac83ca60aae301aa0f0
alska95 <rhsalska55@naver.com>
2021-07-26 15:10:36 +0900

    검증 클래스를 분리하여 컨트롤러 간소화

--- src/main/java/hello/login/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/login/web/validation/ValidationItemControllerV2.java
@@ -26,6 +26,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -151,28 +152,35 @@ public class ValidationItemControllerV2 {
     }
 
     /***********************************************/
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult , RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName = {}" , bindingResult.getObjectName());
         log.info("target ={}" , bindingResult.getTarget());
 
-        //검증 오류 결과를 보관
+        /**
+        * 실행 개요
+         * 1.rejectValue() 호출
+         * 2.MessageCodesResolver을 사용해서 검증 오류 코드로 메시지 코드들을 생성
+         * 3.new FieldError()를 생성하면서 메시지 코드들을 보관
+         * "th:errors"에서 메시지 코드들로 메시지를 순서대로 메시지에서 찾고, 노출*/
 
-        //검증 로직
-        if(!StringUtils.hasText(item.getItemName())){
-            bindingResult.rejectValue("itemName","required"); //조건.객체명.필드명
-       }
-        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
-            bindingResult.rejectValue("price" , "range" , new Object[]{1000 , 1000000} , null);
-       }
-        if(item.getQuantity() == null || item.getQuantity() >= 9999){
-            bindingResult.rejectValue("quantity"  , "max" , new Object[]{9999} , null);
-        }
-        //특정 필드가 아닌 복합 룰 검증 //값이 넘어오는게 아니라 binding실패할 일이 없다.
-        if( item.getPrice()!= null && item.getQuantity() != null && item.getQuantity()*item.getPrice() < 10000){
-            bindingResult.reject("totalPriceMin" , new Object[]{10000, item.getQuantity()*item.getPrice()} , null);
-        }
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /***********************************************/
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult , RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName = {}" , bindingResult.getObjectName());
+        log.info("target ={}" , bindingResult.getTarget());
+
+        //검증 오류 결과를 보관
+        itemValidator.validate(item , bindingResult);
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
@@ -180,7 +188,7 @@ public class ValidationItemControllerV2 {
             return "validation/v2/addForm";
         }
         /**
-        * 실행 개요
+         * 실행 개요
          * 1.rejectValue() 호출
          * 2.MessageCodesResolver을 사용해서 검증 오류 코드로 메시지 코드들을 생성
          * 3.new FieldError()를 생성하면서 메시지 코드들을 보관
