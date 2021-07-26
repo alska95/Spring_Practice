3ce32603c4daeb70713f4884471547a2aaeab855
alska95 <rhsalska55@naver.com>
2021-07-27 00:03:36 +0900

    BeanValidation groups 빈 벨리데이션 한계 극복을 위함

--- src/main/java/hello/login/domain/item/Item.java
+++ src/main/java/hello/login/domain/item/Item.java
@@ -16,17 +16,19 @@ import javax.validation.constraints.NotNull;
 @ScriptAssert(lang = "javascript" , script = "_this.price*_this.quantity >= 10000" , message = "10000원 이상")
 public class Item {
 
+
+    @NotNull(groups = UpdateCheck.class) //Bean Validation 한계 극복
     private Long id;
 
-    @NotBlank(message = "공백 x")
+    @NotBlank(groups = {SaveCheck.class , UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000 , max = 1000000)
+    @NotNull(groups = {SaveCheck.class , UpdateCheck.class})
+    @Range(min = 1000 , max = 1000000 , groups = {SaveCheck.class , UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class , UpdateCheck.class})
+    @Max(value = 9999 , groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
--- src/main/java/hello/login/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/login/web/validation/ValidationItemControllerV3.java
@@ -3,6 +3,8 @@ package hello.login.web.validation;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.item.SaveCheck;
+import hello.login.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -43,8 +45,38 @@ public class ValidationItemControllerV3 {
     }
 
     /***********************************************/
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult , RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName = {}" , bindingResult.getObjectName());
+        log.info("target ={}" , bindingResult.getTarget());
+        /**
+         * 등록된 검증기 하나하나 뒤지고 support되는거 있으면 자동 사용*/
+        //검증 오류 결과를 보관
+//       @validated가 이거 대신 해준다. itemValidator.validate(item , bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("error = {}" , bindingResult);
+            return "validation/v3/addForm";
+        }
+        /**
+         * 실행 개요
+         * 1.rejectValue() 호출
+         * 2.MessageCodesResolver을 사용해서 검증 오류 코드로 메시지 코드들을 생성
+         * 3.new FieldError()를 생성하면서 메시지 코드들을 보관
+         * "th:errors"에서 메시지 코드들로 메시지를 순서대로 메시지에서 찾고, 노출*/
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    /***********************************************/
+    @PostMapping("/add")
+    public String addItemV2(@Validated(value = SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult , RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName = {}" , bindingResult.getObjectName());
         log.info("target ={}" , bindingResult.getTarget());
@@ -79,8 +111,20 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+//    @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item , BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}" , bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(value = UpdateCheck.class) @ModelAttribute Item item , BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()){
             log.info("errors = {}" , bindingResult);
