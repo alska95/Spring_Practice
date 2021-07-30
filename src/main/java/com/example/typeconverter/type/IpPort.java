e7451d85679e7e571ca8708d6d977cc8093eab9f
alska95 <rhsalska55@naver.com>
2021-07-30 21:31:57 +0900

    타입 컨버터 직접 만들어 보기

---
+++ src/main/java/com/example/typeconverter/type/IpPort.java
@@ -0,0 +1,25 @@
+package com.example.typeconverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+
+/**
+ * "127.0.0.1:8080" 같은거 컨버터로 이쁘게 바꿔보자!
+ * */
+@Getter
+@EqualsAndHashCode
+public class IpPort {
+
+
+    private String ip;
+    private int port;
+
+
+    public IpPort(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+
+}
