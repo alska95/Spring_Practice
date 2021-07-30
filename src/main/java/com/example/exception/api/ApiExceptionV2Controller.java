9768d730b2e2d03c5323ac1b6d8efb33bff15bd1
alska95 <rhsalska55@naver.com>
2021-07-30 20:13:53 +0900

    스프링 @ExceptionHandler 사용법

--- src/main/java/com/example/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/com/example/exception/api/ApiExceptionV2Controller.java
@@ -3,21 +3,26 @@ package com.example.exception.api;
 
 import com.example.exception.exception.BadRequestException;
 import com.example.exception.exception.UserException;
+import com.example.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-    @GetMapping("/api/members/{id}")
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //걍 컨트롤러에 붙이는거랑 같음 - 정상응답이기때문에 사용 가능한것!
+    @ExceptionHandler(Exception.class)
+    public ErrorResult illegalExHandler(Exception e){
+        log.error("[exceptionHandler] ex" ,e);
+        return new ErrorResult("ex" , e.getMessage());
+    }
+
+    @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id){
         if(id.equals("ex")){
             throw new RuntimeException("잘못된 사용자");
@@ -28,23 +33,11 @@ public class ApiExceptionController {
         if(id.equals("user-ex")){
             throw new UserException("사용자 오류");
         }
+        if(id.equals("user-ia")){
+            throw new IllegalArgumentException("ExceptionHandler 실험");
+        }
 
-        return new MemberDto(id , "hello " + id);
-    }
-
-    @GetMapping("/api/response-status-ex1")
-    public  String responseStatusEx1(){
-        throw new BadRequestException();
-    }
-
-    @GetMapping("/api/response-status-ex2")
-    public String responseStatusEx2(){
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND , "error.bad" , new IllegalArgumentException());
-    }
-
-    @GetMapping("/api/default-handler-ex")
-    public String defaultException(@RequestParam Integer data){
-        return "ok";
+        return new MemberDto(id , "hello" + id);
     }
 
     @Data
