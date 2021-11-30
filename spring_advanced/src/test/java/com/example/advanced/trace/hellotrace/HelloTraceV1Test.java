deaf28c125b6eca7a62e1e4f0ca666b25b9b075e
alska95 <rhsalska55@naver.com>
2021-11-30 23:33:06 +0900

    로그 추적기 - 적용

--- spring_advanced/src/test/java/com/example/advanced/trace/hellotrace/HelloTraceV1Test.java
+++ spring_advanced/src/test/java/com/example/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -9,6 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class HelloTraceV1Test {
 
+    /**
+     * 이것은 온전한 테스트 코드가 아니다. 일반적으로 테스트라고 함은 자동으로 검증하는 과정이 필요하다.
+     * 이 테스트는 과정이 없고 결과를 콘솔로 직접 확인해야 한다.
+     * 본 테스트는 응답값이 없음.
+     * */
     @Test
     void begin_end(){
         HelloTraceV1 trace = new HelloTraceV1();
