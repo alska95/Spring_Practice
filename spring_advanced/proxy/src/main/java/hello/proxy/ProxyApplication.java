76be625024fd35a58762b0fbd0fa5f770cdb95f9
alska95 <rhsalska55@naver.com>
2021-12-05 00:15:50 +0900

    JDK 동적 프록시 적용 특정 메서드 필터도 적용

--- spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.FieldLogTrace;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -14,7 +16,7 @@ import org.springframework.context.annotation.Import;
 
 
 //@Import({AppV1Config.class, AppV2Config.class, InterfaceProxyConfig.class})
-@Import({ConcreteProxyConfig.class, InterfaceProxyConfig.class})
+@Import({ConcreteProxyConfig.class, DynamicProxyFilterConfig.class})
 @SpringBootApplication (scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
