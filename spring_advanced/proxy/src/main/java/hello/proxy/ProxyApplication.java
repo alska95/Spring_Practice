9d53740e7afc965868d6a11a5970348e149b5130
alska95 <rhsalska55@naver.com>
2021-12-07 23:17:04 +0900

    스프링이 제공하는 빈 후처리기 적용 execution, AspectJ 맛보기

--- spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.FieldLogTrace;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class, InterfaceProxyConfig.class})
 //@Import({ConcreteProxyConfig.class, DynamicProxyFilterConfig.class})
-@Import({ProxyFactoryConfigV1.class, BeanPostProcessorConfig.class})
+//@Import({ProxyFactoryConfigV1.class, BeanPostProcessorConfig.class})
+@Import(AutoProxyConfig.class)
 @SpringBootApplication (scanBasePackages = "hello.proxy.app") //주의
 // 특정 페키지만 제한을 해둬야 우리가 만들어둔 빈 후처리기에 원하는 빈만 들어옴. 아니면 스프링이 쓰는 빈 전부 들어온다..
 public class ProxyApplication {
---
+++ spring_advanced/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,54 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+
+
+//    @Bean
+    public Advisor advisor1(LogTrace logTrace){
+        // 어드바이저 만 빈으로 등록하면 자동 프록시 생성기라는 빈 후처리기에 스프링이 자동으로 등록시켜준다.
+        //pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut(); //aspectj표현식을 쓸 수 있는 포인트컷
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))"); // ..-> app과 app하위의 모든 패키지 | * 모든 메서드 이름
+
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut(); //aspectj표현식을 쓸 수 있는 포인트컷
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))"); // ..-> app과 app하위의 모든 패키지 | * 모든 메서드 이름
+
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
