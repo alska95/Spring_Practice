4e212bfa1f5ac5447079d6efe9d74726e8b9d5dc
alska95 <rhsalska55@naver.com>
2022-04-15 00:15:40 +0900

    jdbc 등록

---
+++ spring_data_access/jdbc/src/main/java/com/example/jdbc/domain/Member.java
@@ -0,0 +1,15 @@
+package com.example.jdbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    private String memberId;
+    private int money;
+
+}
