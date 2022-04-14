a67912a6bcdce16f4c250d5cef11cc65c18ae079
alska95 <rhsalska55@naver.com>
2022-04-15 00:49:08 +0900

    jdbc 조회 - executeQuery, ResultSet

--- spring_data_access/jdbc/src/main/java/com/example/jdbc/repository/MemberRepositoryV0.java
+++ spring_data_access/jdbc/src/main/java/com/example/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import com.example.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -29,6 +30,34 @@ public class MemberRepositoryV0 {
             throw e;
         }finally {
             close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not fount memberId=" + memberId);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw e;
+        }finally {
+            close(con, pstmt, rs);
         }
     }
 
--- spring_data_access/jdbc/src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
+++ spring_data_access/jdbc/src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,29 @@
 package com.example.jdbc.repository;
 
 import com.example.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV2", 10000);
         repositoryV0.save(member);
+
+        //select
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        System.out.println(findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
