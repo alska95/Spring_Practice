0b5d7714e222e320becc3973470e2a6b11783175
alska95 <rhsalska55@naver.com>
2022-06-12 22:02:23 +0900

    트랜잭션 전파 이해 - 전파 예제

--- spring_data_access/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ spring_data_access/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -65,5 +65,40 @@ public class BasicTxTest {
         TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션2 커밋");
         txManager.rollback(tx2);
+
     }
+
+    @Test
+    void inner_commit() {
+
+        /**
+         * 트랜잭션 안에서 동작하는 내부 트랜잭션이 생길경우
+         * 외부 트랜잭션은 물리, 내부 트랜잭션은 논리 트랜잭션이라는 개념을 갖는다. --> required 전파 옵션을 사용할 때 나타난다.
+         * * 원칙 *
+         * 모든 논리 트랜잭션이 커밋되어야 물리 트랜잭션이 커밋된다.
+         * 하나의 논리 트랜잭션이라도 롤백 되면 물리 트랜잭션은 롤백된다.
+         *      --> 물리적으로는 논리가 모두 만족해야 동작
+         * */
+
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        /**
+         * 외부 트랜잭션 적용중 --> 여기에 참여한다. (아무것도 안하겠다는 뜻 - 참여하겠다는 로그만 남김)
+         *                  --> 참여한다는 것은 그냥 그대로 적용받는다는것. 외부 트랜잭션 범위 확장
+         * */
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction()); // --> false
+        log.info("내부 트랜잭션 커밋");
+        //커밋 했는대도 아무 로그도 안나옴. -> 바로 외부 트랜잭션 커밋 --> 아무일도 안한다는 뜻이다.
+        txManager.commit(inner);
+        //하나의 커넥션은 하나만 커밋할 수 있지만, 스프링이 하나의 물리 트랜잭션으로 묶어서 동작하게 한다.
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
+
 }
