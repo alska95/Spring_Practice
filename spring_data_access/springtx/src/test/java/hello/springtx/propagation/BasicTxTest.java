2c31e6adc3537e0182f3f23bcbf1602498d3375f
alska95 <rhsalska55@naver.com>
2022-06-12 20:57:43 +0900

    트랜잭션 전파 이해 - 트랜잭션 두 번 사용

---
+++ spring_data_access/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -0,0 +1,69 @@
+package hello.springtx.propagation;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @TestConfiguration
+    static class Config {
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource){
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @Test
+    void commit(){
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void rollBack(){
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 롤백 시작");
+        txManager.rollback(status);
+        log.info("트랜잭션 롤백 완료");
+    }
+
+    @Test
+    void double_commit(){
+        /**
+         * 1 트랜잭션 종료 후 2 실행
+         * 당연하지만 서로 간섭하지 않음.
+         * 둘은 트랜잭션 풀에서 제공하는 같은 커넥션을 사용한다. (반환 후 재사용)
+        * */
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.rollback(tx2);
+    }
+}
